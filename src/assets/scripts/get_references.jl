929caab4786e40d4add2eeead65a46065996a374
Jesse E <jesse.d.everington@gmail.com>
2026-04-18 21:20:45 -0400

    The card was only being rendered if an image existed.

--- src/assets/scripts/get_references.jl
+++ src/assets/scripts/get_references.jl
@@ -8,9 +8,12 @@ else
           desc = get(x, "description", get(x, "text", ""))
           title = desc |> string
           
-          image === nothing || isempty(image) ? nothing : @htl("""<a title=$(title) class=$(class) href=$(x["url"]) target="_blank">
+          @htl("""<a title=$(title) class=$(class) href=$(x["url"]) target="_blank">
               <h3>$(x["name"])</h3>
-              <img src=$(image)>
+              <p>$(desc)</p>
+              $(if !(image === nothing || isempty(image))
+                  @htl("""<img src=$(image)>""")
+              end)
           </a>""")
       end for x in metadata["homepage"]["references"]
     ]
