7fb31b818c5ac23939f8dbd615c6b3c2f3a4b0f2
Jesse E <jesse.d.everington@gmail.com>
2026-07-01 20:24:11 -0400

    .

--- src/assets/scripts/get_references.jl
+++ src/assets/scripts/get_references.jl
@@ -12,18 +12,6 @@ else
           @htl("""<a title=$(title) class=$(class) href=$(x["url"]) target="_blank">
               <h3>$(x["name"])</h3>
               <p>$(desc)</p>
-              <div style="align: center">
-                    <select id="$(unique_id)-select" class="resource-select" data-resource="$(unique_id)" style="padding: 8px 12px; border-radius: 4px; border: 1px solid #ddd; font-size: 14px;">
-                        <option value="">-- $(get(resource, "select_placeholder", "Choose an option")) --</option>
-                        $([
-                            let opt = option_item
-                                opt_json = JSON.json(opt)
-                                @htl("""<option value='$(opt_json)'>$(opt["name"])</option>""")
-                            end
-                            for option_item in dropdown_options
-                        ])
-                    </select>
-               </div>
               $(if !(image === nothing || isempty(image))
                   @htl("""<img src=$(image)>""")
               end)
